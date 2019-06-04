6758f9af32cbf7db4b1e8d240acf9e99c5b04ef6
Klas Karlsson <klakar@users.noreply.github.com>
2019-06-04 17:48:10 +0200

    Large Leaflet map preview
    Using CSS to make the preview map 100% of the browser window. This is more useful for preview of the maps and is not dependent on screen size or browser window size.

--- tiles_xyz/tiles_xyz_algorithm.py
+++ tiles_xyz/tiles_xyz_algorithm.py
@@ -425,13 +425,13 @@ LEAFLET_TEMPLATE = '''
    integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
    crossorigin=""></script>
   <style type="text/css">
+    body {{
+       margin: 0;
+       padding: 0;
+    }}
     #map{{
-       left: 10px;
-       right: 10px;
-       top: 10px;
-       bottom: 10px;
-       width: 600px; 
-       height: 400px;
+       width: 100%; 
+       height: 100%;
     }}
   </style>
 </head>
