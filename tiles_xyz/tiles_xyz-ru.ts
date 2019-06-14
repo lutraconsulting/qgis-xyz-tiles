cfa3a2214d94619e696a849cbfc1f2e0c0450eee
Petr Tsymbarovich <petr@tsymbarovich.ru>
2019-06-14 09:15:41 +0300

    Add Russian translation

---
+++ tiles_xyz/tiles_xyz-ru.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ru" sourcelanguage="">
+<context>
+    <name>Base</name>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="147"/>
+        <source>Extent</source>
+        <translation>Экстент</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="148"/>
+        <source>Minimum zoom</source>
+        <translation>Минимальный масштаб</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="153"/>
+        <source>Maximum zoom</source>
+        <translation>Максимальный масштаб</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="158"/>
+        <source>DPI</source>
+        <translation>DPI</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="164"/>
+        <source>Tile format</source>
+        <translation>Формат тайлов</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="168"/>
+        <source>Use transparent background (PNG only)</source>
+        <translation>Использовать прозрачность (только PNG)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="171"/>
+        <source>Quality (JPG only)</source>
+        <translation>Качество (только JPG)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="291"/>
+        <source>Invalid zoom levels range.</source>
+        <translation>Неправильный интервал масштабов.</translation>
+    </message>
+</context>
+<context>
+    <name>TilesXYZAlgorithmDirectory</name>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="487"/>
+        <source>Tile width</source>
+        <translation>Ширина тайла</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="492"/>
+        <source>Tile height</source>
+        <translation>Высота тайла</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="497"/>
+        <source>Use inverted tile Y axis (TMS convention)</source>
+        <translation>Инверсия оси Y (формат TMS)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="501"/>
+        <source>Output directory</source>
+        <translation>Выходной каталог</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="504"/>
+        <source>Output html (Leaflet)</source>
+        <translation>Выходной HTML (Leaflet)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="504"/>
+        <source>HTML files (*.html)</source>
+        <translation>Файлы HTML (*.html)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="513"/>
+        <source>Generate XYZ tiles (Directory)</source>
+        <translation>Сгенерировать тайлы XYZ (каталог)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="520"/>
+        <source>You need to specify output directory.</source>
+        <translation>Необходимо указать выходной каталог.</translation>
+    </message>
+</context>
+<context>
+    <name>TilesXYZAlgorithmMBTiles</name>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="387"/>
+        <source>Output file (for MBTiles)</source>
+        <translation>Выходной файл (MBTiles)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="387"/>
+        <source>MBTiles files (*.mbtiles)</source>
+        <translation>Файлы MBTiles (*.mbtiles)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="396"/>
+        <source>Generate XYZ tiles (MBTiles)</source>
+        <translation>Сгенерировать тайлы XYZ (MBTiles)</translation>
+    </message>
+    <message>
+        <location filename="tiles_xyz_algorithm.py" line="401"/>
+        <source>You need to specify output filename.</source>
+        <translation>Необходимо указать имя выходного файла.</translation>
+    </message>
+</context>
+<context>
+    <name>TilesXYZProvider</name>
+    <message>
+        <location filename="tiles_xyz_provider.py" line="74"/>
+        <source>TilesXYZ</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
