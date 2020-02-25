caaa522371a712d06c5d4a03fe880e2c6df300ba
Danilo da Silveira Figueira <danilosfigueira@gmail.com>
2020-02-25 11:33:51 -0300

    Version 1.4
    Remove incompatible class import.

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,17 +1,19 @@
-set PATH=C:\OSGeo4W64\apps\Python37\Scripts\;%PATH%
-SET OSGEO4W_ROOT=C:\OSGeo4W64
+
+set OSGEO=F:\Danilo\Programas\QGIS_34_15
+set PATH=%OSGEO%\apps\Python37\Scripts\;%PATH%
+SET OSGEO4W_ROOT=%OSGEO%
 call "%OSGEO4W_ROOT%"\bin\o4w_env.bat
 call "%OSGEO4W_ROOT%"\apps\grass\grass78\etc\env.bat
 
 path %PATH%;%OSGEO4W_ROOT%\apps\qgis\bin
 path %PATH%;%OSGEO4W_ROOT%\apps\grass\grass78\lib
-path %PATH%;C:\OSGeo4W64\apps\Qt5\bin
-path %PATH%;C:\OSGeo4W64\apps\Python37\Scripts
+path %PATH%;%OSGEO%\apps\Qt5\bin
+path %PATH%;%OSGEO%\apps\Python37\Scripts
 
 set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis\python
 set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python37
 
-set PATH=C:\Danilo\Programas\PortableGit\bin;%PATH%
+set PATH=F:\Danilo\Programas\Git\bin\;%PATH%
 path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
 set GDAL_FILENAME_IS_UTF8=YES
@@ -20,6 +22,7 @@ set VSI_CACHE_SIZE=1000000
 set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 set QGIS_PLUGINPATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 rem easy_install pb_tool
+rem python -m pip install pb_tool
 
 %~d0
 cd %~dp0../mappia_publisher/
