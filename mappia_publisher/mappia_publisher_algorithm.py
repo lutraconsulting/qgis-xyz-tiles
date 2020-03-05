2c04ef9b6336ab2f58f0536d8592dc9339ef0a97
Danilo da Silveira Figueira <danilosfigueira@gmail.com>
2020-03-05 00:50:06 -0300

    Update mappia_publisher_algorithm.py

--- mappia_publisher/mappia_publisher_algorithm.py
+++ mappia_publisher/mappia_publisher_algorithm.py
@@ -90,7 +90,7 @@ def deg2num(lat_deg, lon_deg, zoom):
     lat_rad = math.radians(lat_deg)
     n = 2.0 ** zoom
     xtile = int((lon_deg + 180.0) / 360.0 * n)
-    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+    ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
     return (xtile, ytile)
 
 
